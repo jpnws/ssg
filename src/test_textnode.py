e302501da1d567b7bf3de39baf0202addcb5497c
Ji Park <ji.park@jpnws.com>
2024-05-30 09:54:47 -0700

    Add test cases (unit tests) for textnode.py

---
+++ src/test_textnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from textnode import TextNode
+
+
+class TestTextNode(unittest.TestCase):
+
+    def test_init(self):
+        text = "This is a text node"
+        text_type = "bold"
+        url = "https://www.github.com"
+        node1 = TextNode(text, text_type, url)
+        self.assertEqual(node1.text, text)
+        self.assertEqual(node1.text_type, text_type)
+        self.assertEqual(node1.url, url)
+
+    def test_eq(self):
+        node1 = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node1, node2)
+
+    def test_neq(self):
+        node1 = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node.", "italic")
+        self.assertNotEqual(node1, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
