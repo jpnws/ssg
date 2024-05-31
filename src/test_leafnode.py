a02ea01dc1a938efd1be2e1c641bec7fd3a39821
Ji Park <ji.park@jpnws.com>
2024-05-30 23:42:31 -0700

    Refactor test code for better maintainability.

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -5,13 +5,54 @@ from leafnode import LeafNode
 
 class TestLeafNode(unittest.TestCase):
 
-    def test_to_html(self):
-        leafnode1 = LeafNode("p", "This is a paragraph.", {"id": "para"})
+    def setUp(self) -> None:
+        self.p_tag = "p"
+        self.p_value = "This is a paragraph."
+        self.p_props = {"id": "para"}
 
-        self.assertEqual(leafnode1.tag, "p")
-        self.assertEqual(leafnode1.value, "This is a paragraph.")
-        self.assertIsNone(leafnode1.children)
-        self.assertEqual(leafnode1.props, {"id": "para"})
+    def test_to_html_vars(self):
+        """
+        Test for the instance variables.
+        """
+        # Arrange / Act
+        leafnode = LeafNode(self.p_tag, self.p_value, self.p_props)
+        # Assert
+        self.assertEqual(leafnode.tag, self.p_tag)
+        self.assertEqual(leafnode.value, self.p_value)
+        self.assertIsNone(leafnode.children)
+        self.assertEqual(leafnode.props, self.p_props)
 
-        html = leafnode1.to_html()
-        self.assertEqual(html, '<p id="para">This is a paragraph.</p>')
+    def test_to_html_output(self):
+        """
+        Test for the full HTML string returned by to_html.
+        """
+        # Arrange
+        leafnode = LeafNode(self.p_tag, self.p_value, self.p_props)
+        # Act
+        html = leafnode.to_html()
+        # Assert
+        self.assertEqual(
+            html,
+            f'<{self.p_tag} id="{self.p_props["id"]}">{self.p_value}</{self.p_tag}>',
+        )
+
+    def test_to_html_value_error(self):
+        """
+        Test for the ValueError exception when value is None.
+        """
+        # Arrange / Act
+        leafnode = LeafNode(self.p_tag, None, self.p_props)
+        # Assert
+        with self.assertRaises(ValueError):
+            leafnode.to_html()
+
+    def test_to_html_plain_text_output(self):
+        """
+        Test for the plain text value when tag is empty or None.
+        """
+        # Arrange
+        leafnode = LeafNode(None, self.p_value, self.p_props)
+        # Act
+        res = leafnode.to_html()
+        # Assert
+        self.assertEqual(res, self.p_value)
