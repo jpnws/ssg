3e376608540ce07b6fcb63f929f8c0508a484ce7
Ji Park <ji.park@jpnws.com>
2024-05-31 09:34:38 -0700

    Refactor tests with comments.

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -4,28 +4,50 @@ from textnode import TextNode
 
 
 class TestTextNode(unittest.TestCase):
+    def setUp(self) -> None:
+        self.text = "This is a text node"
+        self.text_type1 = "bold"
+        self.text_type2 = "italic"
+        self.url = "https://www.github.com"
 
     def test_init(self):
-        text = "This is a text node"
-        text_type = "bold"
-        url = "https://www.github.com"
-        node1 = TextNode(text, text_type, url)
-        self.assertEqual(node1.text, text)
-        self.assertEqual(node1.text_type, text_type)
-        self.assertEqual(node1.url, url)
+        """
+        Test that the TextNode instance variables are correctly set.
+        """
+        # Arrange / Act
+        node1 = TextNode(self.text, self.text_type1, self.url)
+        # Assert
+        self.assertEqual(node1.text, self.text)
+        self.assertEqual(node1.text_type, self.text_type1)
+        self.assertEqual(node1.url, self.url)
 
     def test_eq(self):
-        node1 = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold")
+        """
+        Test that two TextNode instances are equal.
+        """
+        # Arrange / Act
+        node1 = TextNode(self.text, self.text_type1)
+        node2 = TextNode(self.text, self.text_type1)
+        # Assert
         self.assertEqual(node1, node2)
 
     def test_neq(self):
-        node1 = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node.", "italic")
+        """
+        Test that two TextNode instances are not equal.
+        """
+        # Arrange / Act
+        node1 = TextNode(self.text, self.text_type1)
+        node2 = TextNode(self.text, self.text_type2)
+        # Assert
         self.assertNotEqual(node1, node2)
 
     def test_none_url(self):
-        node1 = TextNode("This is a text node", "bold")
+        """
+        Test that the url attribute is None when not provided.
+        """
+        # Arrange / Act
+        node1 = TextNode(self.text, self.text_type1)
+        # Assert
         self.assertIsNone(node1.url)
 
 
