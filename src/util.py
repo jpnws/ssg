a41122229933069766fd70b45f66408303bd5a3e
Ji Park <ji.park@jpnws.com>
2024-06-01 11:48:51 -0700

    add text_node_to_html_node util function.

---
+++ src/util.py
@@ -0,0 +1,19 @@
+from textnode import TextNode
+from htmlnode import HTMLNode
+from leafnode import LeafNode
+
+
+def text_node_to_html_node(text_node: TextNode) -> HTMLNode:
+    if text_node.text_type == "text":
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == "bold":
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == "italic":
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == "code":
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == "link":
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == "image":
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError("Invalid text type.")
