461b02fe595add6261c734c6f09ab78a96f951ab
Ji Park <ji.park@jpnws.com>
2024-06-03 21:30:43 -0700

    Add comments.

--- src/util.py
+++ src/util.py
@@ -200,12 +200,35 @@ def splitter(text: str, delim: str, text_type: str) -> list[TextNode]:
 
 
 def extract_markdown_images(text: str) -> list[tuple[str, str]]:
+    """
+    Helper function that extracts the alt text and the url from the markdown
+    syntax for images.
+
+    Args:
+        text (str): The markdown text from which to extract alt text and url of
+        images.
+
+    Returns:
+        list[tuple[str, str]]: A list of tuples where the tuple has two elements
+        - the first one being the alt text and the second one the url.
+    """
     r = r"!\[(.*?)\]\((.*?)\)"
     ret = re.findall(r, text)
     return ret
 
 
 def extract_markdown_links(text: str) -> list[tuple[str, str]]:
+    """
+    Helper function that extracts the link text and its url from the markdown
+    syntax for links.
+
+    Args:
+        text (str): The markdown text from which to extract link text and url.
+
+    Returns:
+        list[tuple[str, str]]: A list of tuples where the tuple has two elements
+        - the first one is the link text and the second is the url.
+    """
     r = r"\[(.*?)\]\((.*?)\)"
     ret = re.findall(r, text)
     return ret
