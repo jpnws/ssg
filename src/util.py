d5f2e3dfb5c2260b9271d80c4bad606fb5764ac2
Ji Park <ji.park@jpnws.com>
2024-06-05 17:51:06 -0700

    remove bootdev solution.

--- src/util.py
+++ src/util.py
@@ -33,27 +33,6 @@ def text_node_to_html_node(text_node: TextNode) -> HTMLNode:
     raise ValueError("Invalid text type.")
 
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_nodes = []
-    for old_node in old_nodes:
-        if old_node.text_type != text_type_text:
-            new_nodes.append(old_node)
-            continue
-        split_nodes = []
-        sections = old_node.text.split(delimiter)
-        if len(sections) % 2 == 0:
-            raise ValueError("Invalid markdown, formatted section not closed")
-        for i in range(len(sections)):
-            if sections[i] == "":
-                continue
-            if i % 2 == 0:
-                split_nodes.append(TextNode(sections[i], text_type_text))
-            else:
-                split_nodes.append(TextNode(sections[i], text_type))
-        new_nodes.extend(split_nodes)
-    return new_nodes
-
-
 def extract_markdown_images(text: str) -> list[tuple[str, str]]:
     """
     Helper function that extracts the alt text and the url from the markdown
