4425912ceec271e8f52985f9fe406eec0ecee3f1
Ji Park <ji.park@jpnws.com>
2024-06-06 22:38:26 -0700

    Start of block nodes.

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -11,25 +11,20 @@ def markdown_to_blocks(markdown: str) -> list[str]:
     Returns:
         - list[str]: A list of all the blocks with markdown text.
     """
-    # First, split the entire text by blocks.
-    markdown_blocks: list[str] = markdown.split("\n\n")
-    # Second, strip whitespaces around each block.
-    stripped_blocks: list[str] = []
-    for line in markdown_blocks:
-        if line:
-            stripped_blocks.append(line.strip())
-    # Third, strip whitespace from every line from every block.
-    line_stripped_blocks: list[str] = []
-    for block in stripped_blocks:
-        if block:
-            line_stripped: list[str] = []
-            for block_line in block.split("\n"):
-                line_stripped.append(block_line.strip())
-            line_stripped_blocks.append("\n".join(line_stripped))
-    return line_stripped_blocks
-    # Most likely a complicated approach.
-    # return [
-    #     "\n".join([block_line.strip() for block_line in block.split("\n")])
-    #     for block in [line.strip() for line in markdown.split("\n\n") if line]
-    #     if block
-    # ]
+    print(repr(markdown))
+    # # First, split the entire text by blocks.
+    # markdown_blocks: list[str] = markdown.split("\n\n")
+    # # Second, strip whitespaces around each block.
+    # stripped_blocks: list[str] = []
+    # for line in markdown_blocks:
+    #     if line:
+    #         stripped_blocks.append(line.strip())
+    # # Third, strip whitespace from every line from every block.
+    # line_stripped_blocks: list[str] = []
+    # for block in stripped_blocks:
+    #     if block:
+    #         line_stripped: list[str] = []
+    #         for block_line in block.split("\n"):
+    #             line_stripped.append(block_line.strip())
+    #         line_stripped_blocks.append("\n".join(line_stripped))
+    # return line_stripped_blocks
--- test/test_markdown_blocks.py
+++ test/test_markdown_blocks.py
@@ -4,12 +4,41 @@ from markdown_blocks import markdown_to_blocks
 
 
 class TestMarkdownBlocks(unittest.TestCase):
-    def test_markdown_to_blocks(self):
+    #     def test_markdown_to_blocks(self):
+    #         # Arrange
+    #         text = """
+
+    # This is **bolded** paragraph
+
+    # This is another paragraph with *italic* text and `code` here
+    # This is the same paragraph on a new line
+
+    # * This is a list
+    # * with items
+
+    # """
+    #         expected = [
+    #             "This is **bolded** paragraph",
+    #             "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+    #             "* This is a list\n* with items",
+    #         ]
+    #         # Act
+    #         actual = markdown_to_blocks(text)
+    #         # Assert
+    #         self.assertListEqual(actual, expected)
+
+    def test_markdown_code_block(self):
         # Arrange
         text = """
 
-
 This is **bolded** paragraph
+
+```python
+
+
+def func1(arg):
+    pass
+```
 
 This is another paragraph with *italic* text and `code` here
 This is the same paragraph on a new line
@@ -17,27 +46,27 @@ This is the same paragraph on a new line
 * This is a list
 * with items
 
-
-"""
-        expected = [
-            "This is **bolded** paragraph",
-            "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
-            "* This is a list\n* with items",
-        ]
-        # Act
-        actual = markdown_to_blocks(text)
-        # Assert
-        self.assertListEqual(actual, expected)
-
-    def test_markdown_code_block(self):
-        # Arrange
-        text = """
 ```python
 
 
-def func(arg):
+def func2(arg):
+    pass
+
+def func3(arg):
     pass
 ```
+
+> This is a quote
+> with greater than symbol.
+
+## This is a heading
+
+An ordered list:
+1. One
+2. Two
+3. Three
+
+
         """
         expected = [
             "```python\ndef func(arg):\n    pass\n```",
